dfc07c0c2be751d067a1ca0c74db22bcb16032ce
Henry Jackson <tjcoder@protonmail.com>
2025-12-02 09:03:00 +0000

    Day 2 - Part two - repetitions 2 or more inclusive of first one

--- 2/part_two.py
+++ 2/part_two.py
@@ -1,19 +1,18 @@
 # 23778731601 - too low
 # 25344579921 - too low
+# 66500947346 - correct
 
 def is_invalid(strid):
     search_len = 1
     while search_len <= len(strid) >> 1:
-        if search_len * 2 >= len(strid):
-            search_len += 1
-            continue
-
         term = strid[0:search_len]
         repetitions = 0
         for t in range(1, len(strid) // search_len):
             if strid[t * search_len:(t+1) * search_len] == term:
                 repetitions += 1
-        if ((repetitions + 1) * search_len) == len(strid):
+        
+        # Repetitions are the entire string, and there are 2 or more
+        if ((repetitions + 1) * search_len) == len(strid) and repetitions >= 1:
             return True
 
         search_len += 1
@@ -42,10 +41,10 @@ def process():
         id_ranges = input.split(',')
         for id_range in id_ranges:
             invalid_ids = get_invalid_ids_in_range(*parse_id_range(id_range))
-            #if len(invalid_ids):
-            #    print(f'In range {id_range} invalid ids are {invalid_ids}')
-            if len(invalid_ids) == 0:
-                print(f'None found in range {id_range}')
+            if len(invalid_ids):
+                print(f'In range {id_range} invalid ids are {invalid_ids}')
+            #if len(invalid_ids) == 0:
+            #    print(f'None found in range {id_range}')
             total += sum(invalid_ids)
 
     print(f'Total is {total}')
