05c61ce5cc6d4e7e10aa3436b0d265f5ea1b97c1
Henry Jackson <tjcoder@protonmail.com>
2025-12-05 19:14:19 +0000

    Some comments

--- 3/part_two.py
+++ 3/part_two.py
@@ -4,21 +4,21 @@ def evaluate_from_start_index(batteries, start_index, digit):
     if digit >= digits_needed:
         return None
 
-    from_end = digits_needed - 1 - digit
-
-    if digit == digits_needed - 1:
-        search_array = batteries[start_index:]
-    else:
-        search_array = batteries[start_index:-from_end]
+    # ignore last few digits to ensure we don't run out
+    from_end = digits_needed - 1 - digit    
+    search_array = batteries[start_index:len(batteries)-from_end]
 
     max_digit = max(search_array)
     next_start_index = start_index + search_array.index(max_digit) + 1
 
+    # search for next digit
     next_result = evaluate_from_start_index(batteries, next_start_index, digit + 1)
     
+    # end of recursion
     if next_result == None:
         return max_digit
 
+    # concatenate result string
     return max_digit + next_result
 
 def process():
@@ -27,7 +27,6 @@ def process():
         for line in file:            
             batteries = line.strip()
             value = int(evaluate_from_start_index(batteries, 0, 0))
-            print(f'Twelve {value}')
             total += value
             
     print(f'Result is {total}') 
